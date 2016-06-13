48a4c3ee2313569fff05e6f41ef2b724dec59ed6
nfrasser <nfrasser@gmail.com>
2016-06-12 23:04:08 -0400

    Initial commit of home app HTML
    Setting up Django to show mocked-up HTML. Very basic so far
    The default route is configured to bring up this app

---
+++ home/assets/home/js/main.js
@@ -0,0 +1,21 @@
+jQuery(function($) {
+
+  $('.social-feed-container').socialfeed({
+    instagram: {
+      accounts: ['@mathorientation'],
+      limit: 2,
+      client_id: '83a26336707148e7ad61db46e8707182',
+      access_token: '1679175523.83a2633.d1380d693ce14ea1bd06b926723ca2f7'
+    },
+    twitter: {
+      accounts: ['@MATHorientation'],
+      limit: 2,
+      consumer_key: 'GeFXep7qANgeRhnAHL1e7D48S',
+      consumer_secret: 'xPkEoBe616De9tLcP8Y7pRetc3QMeOFkapnOWvIM6ss8L8ktf9'
+    },
+    length: 140,
+    template: '/assets/home/social-feed-template.html',
+    show_media: true
+  });
+
+});
