2c432d3ba62bbad6e5614dc2c279c63c892150ec
nfrasser <nfrasser@gmail.com>
2016-06-23 00:35:07 -0400

    Additional styles for site layout and footer
    Includes proper links and skeleton content for required pages

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = (
-    url(r'^/', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^news/', views.news, name='news'),
     url(r'^about/', views.about, name='about'),
     url(r'^resources/', views.resources, name='resources'),
