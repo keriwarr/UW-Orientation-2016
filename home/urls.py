5b81edea172803c5d7034da4885a9b7ca97318fa
nfrasser <nfrasser@gmail.com>
2016-06-22 21:04:39 -0400

    Setting up website routes
    Includes all items in the navigation bar

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = (
-    url(r'^$', views.index, name='index'),
+    url(r'^/', views.index, name='index'),
+    url(r'^news/', views.news, name='news'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^resources/', views.resources, name='resources'),
+    url(r'^faq/', views.faq, name='faq'),
+    url(r'^contact/', views.contact, name='contact'),
 )
