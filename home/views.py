2dc14612fa747ec2d52062f412bd1c7e0f4f7fbf
nfrasser <nfrasser@gmail.com>
2016-06-22 21:04:39 -0400

    Setting up website routes
    Includes all items in the navigation bar

--- home/views.py
+++ home/views.py
@@ -3,10 +3,25 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html', context=data())
 
+def news(request):
+    return render(request, 'home/news.html', context=data())
+
+def about(request):
+    return render(request, 'home/about.html', context=data())
+
+def resources(request):
+    return render(request, 'home/resources.html', context=data())
+
+def faq(request):
+    return render(request, 'home/faq.html', context=data())
+
+def contact(request):
+    return render(request, 'home/contact.html', context=data())
+
 def data():
     return {
         'site': {
-            'title': 'Math Orientation 2016',
+            'title': 'Math Orientation 2016: Mathemagical Kingdoms',
             'email': 'mathorientation@uwaterloo.ca',
             'description': """
                 Official website of the Univeristy of Waterloo Math Faculty Orientation Week
