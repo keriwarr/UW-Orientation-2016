3e2be2e17a31d7695cb669155ac37a15b70a36aa
Ben Zhang <bzhang@500px.com>
2016-06-24 21:54:28 -0400

    Make downward arrow clickable

--- home/assets/home/js/main.js
+++ home/assets/home/js/main.js
@@ -18,6 +18,21 @@ jQuery(function($) {
     show_media: true
   });
 
+  // Smooth scrolling for same-page elements
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') &&
+        location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   var $header = $('#default-header');
   if ($header.length < 1) {
     return;
