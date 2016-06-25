915c6dbb11cd1f4b9bdc51ead514a91f23f0597d
Ben Zhang <ben-z@users.noreply.github.com>
2016-06-25 00:22:30 -0400

    Make downward arrow clickable (#11)
    * Make downward arrow clickable
    
    * Add attribution

--- home/assets/home/js/main.js
+++ home/assets/home/js/main.js
@@ -18,6 +18,22 @@ jQuery(function($) {
     show_media: true
   });
 
+  // Smooth scrolling for same-page elements
+  // Source: http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') &&
+        location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   var $header = $('#default-header');
   if ($header.length < 1) {
     return;
